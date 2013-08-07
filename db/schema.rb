a5a4bb1d598a02f870472b631486a09138062337
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-07 15:22:03 +0100

    New schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130807111106) do
+ActiveRecord::Schema.define(:version => 20130807141808) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 20130807111106) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.integer  "user_id"
+  end
+
+  create_table "tasks_users", :force => true do |t|
+    t.integer "task_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
