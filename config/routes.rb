df4f684326ee431b3cd7b900773e069f1683a8e0
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-07 14:34:44 +0100

    wip task 3.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Checklist::Application.routes.draw do
   devise_for :users
 
   resources :tasks
-  match '/tasks/:id/assign' => 'tasks#assign', :via => :post
+  match '/tasks/:id' => 'tasks#assign', :via => :post
   root to: 'tasks#index'
 end
