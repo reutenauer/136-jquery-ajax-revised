5f217d84796d41d7628b07793ab0b915b003a2fe
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-07 14:41:31 +0100

    Added method #complete? to model Task.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
+# encoding: UTF-8
+
 class Task < ActiveRecord::Base
   attr_accessible :complete, :name, :user_id
   belongs_to :user
+
+  # It makes me happy to be able to say “if tasks.complete?”
+  def complete?
+    complete
+  end
 
   def assign(assignee)
     self.user = assignee
