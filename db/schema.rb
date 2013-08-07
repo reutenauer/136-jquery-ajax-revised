fdb893ad050d9d8c6f60924e7aa5f47aaf972207
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-08-07 12:22:56 +0100

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130807102900) do
+ActiveRecord::Schema.define(:version => 20130807111106) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.boolean  "complete",   :default => false, :null => false
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
